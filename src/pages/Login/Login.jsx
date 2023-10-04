9adabbe4a5a74eb3333083e51349a95e4ecebd71
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-10-04 09:47:47 +0600

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3 className="text-4xl">This login page  </h3>
+        </div>
+    );
+};
+
+export default Login;
